53a5731d0424714e494704cb01c7fca1170b9f6d
jake.liu <jake.liu@baozun.com>
2022-09-06 23:43:51 +0800

    numpy 数组转矩阵测试

---
+++ numpy.md
@@ -0,0 +1,12 @@
+# 创建虚拟环境
+```
+https://docs.python.org/zh-cn/3/library/venv.html
+
+python3 -m venv .venv
+```
+
+# numpy安装
+
+```
+pip install numpy
+```
---
+++ numpy_example.py
@@ -0,0 +1,12 @@
+#!.venv/bin/python
+
+import numpy as np
+A = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+arr1 = np.array(A) # 将列表转为矩阵
+print("A=",A)
+print("通过列表A创建的矩阵arr1\n",arr1)
+
+B=((1,2,3,4),(5,6,7,8),(9,10,11,12))
+arr2 = np.array(B) # 将元组转为矩阵
+print("B=",B)
+print("通过列表A创建的矩阵arr2\n",arr2)
