ad7d1da5470b783d9d708bda28cc079cf27e7b87
jake.liu <jake.liu@baozun.com>
2023-05-21 15:38:34 +0800

    kmeans

--- cpu_promethues_machine_leanring.py
+++ cpu_promethues_machine_leanring.py
@@ -5,8 +5,11 @@ from pprint import pprint
 import requests
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Prometheus API地址
+from cpu_machine_leanring import kmeans
+
 url = "http://localhost:9090/api/v1/query_range"
 
 # 查询语句
@@ -14,7 +17,7 @@ query = "100 - (avg by (instance) (rate(node_cpu_seconds_total{job='linux-node',
 
 # 中国时间字符串
 china_start_time_str = '2023-05-21 10:00:00'
-china_end_time_str = '2023-05-21 12:00:00'
+china_end_time_str = '2023-05-21 17:00:00'
 
 # 将字符串转换为 datetime 对象
 china_start_time = datetime.datetime.strptime(china_start_time_str, '%Y-%m-%d %H:%M:%S')
@@ -67,14 +70,33 @@ for s in series:
 
 #
 # # 将Series转换为DataFrame
-# df = pd.DataFrame(columns=['timestamp', 'value'])
-# for s in series:
-#     df_temp = pd.DataFrame(s['values'], columns=['timestamp', 'value'])
-#     df_temp['instance'] = s['metric']['instance']
-#     df = pd.concat([df, df_temp])
-#
-# # 转换时间戳为datetime格式
-# df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-#
-# # 计算平均值和标准差
-# df.groupby('instance').agg({'value': ['mean', 'std']})
+df = pd.DataFrame(columns=['timestamp', 'value'])
+for s in series:
+    df_temp = pd.DataFrame(s['values'], columns=['timestamp', 'value'])
+    df_temp['instance'] = s['metric']['instance']
+    """
+    pd.concat用于将多个Pandas数据帧（DataFrame）或序列（Series）沿着指定的轴（axis）连接在一起，生成一个新的数据帧或序列。它可以用于数据的合并、拼接和连接，常用于数据清洗、数据预处理和数据分析等领域。其中，axis参数默认为0，表示沿着行方向进行连接，如果设置为1，则表示沿着列方向进行连接。
+    """
+    df = pd.concat([df, df_temp])
+print(df)
+
+kmeans.fit(df[['value']])
+labels = kmeans.labels_
+print(labels)
+for cluster_id in range(3):
+    cluster_df = df[df['value'] == cluster_id]
+    mean_usage = cluster_df['value'].mean()
+    if mean_usage >= 80:
+        threshold = 90
+    elif mean_usage >= 50:
+        threshold = 70
+    else:
+        threshold = 50
+    print(threshold)
+
+plt.scatter(df['timestamp'],df['instance'], c=labels)
+plt.title('KMeans Clustering')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
